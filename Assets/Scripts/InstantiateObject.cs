f8de4f159336f59364ee5aa7d3f03a8df238908e
Patron Fredo <fredopalacios7@gmail.com>
2023-03-12 11:26:52 -0500

    First commit

---
+++ Assets/Scripts/InstantiateObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateObject : MonoBehaviour
+{
+    //  Variables used to access prefab and start position
+    public Rigidbody foodPrefab;
+    public Transform startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  Instantiate object when user presses "Fire2" button
+        if(Input.GetButtonDown("Fire2"))
+        {
+            Rigidbody newInstance;
+
+            newInstance = Instantiate(foodPrefab, startPosition.position, startPosition.rotation);
+
+            newInstance.AddForce(startPosition.forward * 50);
+        }
+    }
+}
